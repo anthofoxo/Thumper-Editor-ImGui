92e0567c9fdf4736134d688f9ac1e3ff1849f509
AnthoFoxo <anthofoxo@gmail.com>
2024-12-09 05:26:40 -0500

    move audio to own cpp/hpp

--- editor/source/te_entry.cpp
+++ editor/source/te_entry.cpp
@@ -5,6 +5,8 @@
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
 
+#include "te_audio.hpp"
+
 #include <stb_image.h>
 #include <miniaudio.h>
 
@@ -60,37 +62,6 @@ struct Level {
     std::string difficulty;
     std::string description;
     std::string author;
-};
-
-struct AudioEngine final {
-    void init() {
-        ma_context_init(nullptr, 0, nullptr, &mContext);
-
-        ma_device_config deviceConfig = ma_device_config_init(ma_device_type_playback);
-        deviceConfig.playback.format = ma_format_f32;
-        deviceConfig.dataCallback = [](ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount) -> void {
-            ma_engine_read_pcm_frames(reinterpret_cast<ma_engine*>(pDevice->pUserData), pOutput, frameCount, nullptr);
-        };
-        deviceConfig.pUserData = &mEngine;
-
-        ma_device_init(&mContext, &deviceConfig, &mDevice);
-        ma_device_start(&mDevice);
-
-        ma_engine_config engineConfig = ma_engine_config_init();
-        engineConfig.pDevice = &mDevice;
-
-        ma_engine_init(&engineConfig, &mEngine);
-    }
-
-    void uninit() {
-        ma_engine_uninit(&mEngine);
-        ma_device_uninit(&mDevice);
-        ma_context_uninit(&mContext);
-    }
-
-    ma_context mContext;
-    ma_device mDevice;
-    ma_engine mEngine;
 };
 
 uint32_t hash32(unsigned char const* array, unsigned int size) {
